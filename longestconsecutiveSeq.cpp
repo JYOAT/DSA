0a29bcfcee5a6a9fdc1f2a9041da017a50760240
JYOAT <126136692+JYOAT@users.noreply.github.com>
2024-10-04 23:11:17 +0530

    Create longestconsecutiveSeq.cpp

---
+++ longestconsecutiveSeq.cpp
@@ -0,0 +1,29 @@
+int longestSuccessiveElements(vector<int>&a) {
+    // Write your code here.
+     int n = a.size();
+    if (n == 0) return 0;
+
+    //sort the array:
+    sort(a.begin(), a.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (a[i] - 1 == lastSmaller) {
+            //a[i] is the next element of the
+            //current sequence.
+            cnt += 1;
+            lastSmaller = a[i];
+        }
+        else if (a[i] != lastSmaller) {
+            cnt = 1;
+            lastSmaller = a[i];
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+
+//tc=O(N)
