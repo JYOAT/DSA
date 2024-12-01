34063b39fa33c45496339e7117028136f643656e
JYOAT <126136692+JYOAT@users.noreply.github.com>
2024-12-01 20:22:11 +0530

    Update LinkedListDelete.cpp

--- LinkedListDelete.cpp
+++ LinkedListDelete.cpp
@@ -5,11 +5,13 @@ struct Node{
     public:
     int data;
     Node* next;
-    
+
+    public:
     Node(int data1, Node* next1){
         data = data1;
         next = next1;
     }
+    public:
     Node(int data1){
         data = data1;
         next = nullptr;
